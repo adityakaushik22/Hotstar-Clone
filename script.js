9764205cf3db9d8a408b7d0768043974eab542da
Aditya Kaushik <adityakaushik373@gmail.com>
2023-02-20 21:50:08 +0530

    video-cards

--- script.js
+++ script.js
@@ -78,3 +78,19 @@ setInterval(()=>{
     createslide()
 },3000)
 
+
+
+// video
+
+const videocards = [...document.querySelectorAll('.video-card')]
+
+videocards.forEach(item=>{
+    item.addEventListener('mouseover',()=>{
+        let video = item.children[1]
+        video.play()
+    })
+    item.addEventListener('mouseleave',()=>{
+        let video = item.children[1]
+        video.pause()
+    })
+})
